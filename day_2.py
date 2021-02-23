462246872ac929568a72a0e522de76cb40564018
Milorad Bozic <miloradbozic29@gmail.com>
2021-02-23 11:03:27 +0100

    refactor

--- day_2.py
+++ day_2.py
@@ -7,9 +7,7 @@ def decimal_to_binary(number):
     if number == 0:
         return "0"
 
-    exp = 0
-    while number >= 2 ** exp:
-        exp += 1
+    exp = find_highest_exponent(number)
 
     res = ""
     while exp > 0:
@@ -21,6 +19,13 @@ def decimal_to_binary(number):
             res += "0"
 
     return res
+
+
+def find_highest_exponent(number):
+    exp = 0
+    while number >= 2 ** exp:
+        exp += 1
+    return exp
 
 
 assert decimal_to_binary(9) == "1001"
