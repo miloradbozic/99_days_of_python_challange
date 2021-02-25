385351eb77ffa03b9d5c7ba3924c2c67de632541
Milorad Bozic <miloradbozic29@gmail.com>
2021-02-25 11:05:33 +0100

    tiangles

--- day_4.py
+++ day_4.py
@@ -28,3 +28,44 @@ assert longest_binary_gap(529) == 4
 assert longest_binary_gap(20) == 1
 assert longest_binary_gap(15) == 0
 assert longest_binary_gap(0) == 0
+
+
+def factorial(N):
+    factorial = 1
+    for i in range(1, N+1):
+        factorial *= i
+    return factorial
+
+
+assert factorial(1) == 1
+assert factorial(2) == 2
+assert factorial(5) == 120
+assert factorial(6) == 720
+
+
+def triangle(N):
+    for i in range(1, N+1):
+        for j in range(i):
+            print('*', end="")
+        print('')
+
+
+def symmetric_triangle(N):
+    res = ""
+    for i in range(N):
+        for j in range(i):
+
+            res += ' '
+        for k in range(N*2-1 - i*2):
+            res += '*'
+        res += '\n'
+    return res
+
+
+assert symmetric_triangle(6) == """***********
+ *********
+  *******
+   *****
+    ***
+     *
+"""
