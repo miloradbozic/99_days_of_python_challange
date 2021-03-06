a6600a6719fe8e8a3aa98b3545f16b733e9fb757
Milorad Bozic <miloradbozic29@gmail.com>
2021-03-07 00:20:05 +0100

    added faster solution

--- day_11.py
+++ day_11.py
@@ -3,6 +3,7 @@ Write a function that, given an array A of N integers,
 returns the smallest positive integer (greater than 0) that does not occur in A.
 """
 
+# O(n^2)
 def get_smallest_positive(A):
     smallest = 1
     i = 0
@@ -16,5 +17,16 @@ def get_smallest_positive(A):
     return smallest
 
 
-assert get_smallest_positive([1, 3, 6, 4, 1, 2]) == 5
-assert get_smallest_positive([-2, -1]) == 1
+# O(n) + O(m)
+def get_smallest_positive2(A):
+    values = [False] * 100000
+    for v in A:
+        values[v-1] = True
+
+    for i in range(len(values)):
+        if not values[i]:
+            return i+1
+
+
+assert get_smallest_positive2([1, 3, 6, 4, 1, 2]) == 5
+assert get_smallest_positive2([-2, -1]) == 1
