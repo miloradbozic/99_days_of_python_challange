ce22f7442201ab3fb3b579b7699185c4b3d486b0
Milorad Bozic <miloradbozic29@gmail.com>
2021-03-20 12:38:05 +0100

    day 16

---
+++ day16.py
@@ -0,0 +1,22 @@
+'''
+PassingCars
+'''
+
+def solution(A):
+    prefix_sums = get_prefix_sums(A)
+    passing_cars_sum = 0
+    for i in range(len(A)):
+        if A[i] == 0:
+            passing_cars_sum += prefix_sums[len(A)] - prefix_sums[i]
+
+    return passing_cars_sum
+
+def get_prefix_sums(A):
+    K = [0] * (len(A) + 1)
+    for i in range(len(A)):
+        K[i+1] = K[i] + A[i]
+    return K
+
+
+solution([0, 1, 0, 1, 1]) == 5
+
